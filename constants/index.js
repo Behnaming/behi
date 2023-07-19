5c269a2859edcc83632e403110132d5a9b2b4046
Behnaming <90547469+Behnaming@users.noreply.github.com>
2023-07-19 15:34:39 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x1a0f6495abd9dc1dcbcd99ff3db068d0601f6da0";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
